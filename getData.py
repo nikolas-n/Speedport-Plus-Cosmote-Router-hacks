7b03b17c8fbfc9cf0c723774c489300f1b81d00e
Nikolas <nikolas@zmail.gr>
2021-05-26 22:57:25 +0300

    get data with python

--- getData.py
+++ getData.py
@@ -1,15 +1,38 @@
 #!/bin/python3
 
 import requests
+import os
+import urllib3
+import json
 
-headers = {'Accept-Language': 'en','Cookie':'session_id='}
+# Suppress loggin about self-signed certificate
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+# Set headers
+headers = {'Accept-Language': 'en'}
+
+# Start session
+session = requests.Session()
+
+# Set username and password
+login_data = {'showpw':'0','username':'**********','password':'********'}
+
+# Login
+session.post('https://192.168.1.1/data/Login.json',data=login_data,headers=headers,verify=False)
+
+# Load endpoints
 with open('endpoints_list.txt') as endpoints_list:
 	endpoints = endpoints_list.readlines()
 
+# Create directory if it doesn't exist
+if not os.path.exists('my_router_responses'):
+    os.makedirs('my_router_responses')
+
+# Download JSONs
 for endpoint in endpoints:
 	endpoint = endpoint.strip('\n')
 	print('Retrieving endpoint '+endpoint)
-	url='http://192.168.1.1/data/'+endpoint
-	request = requests.get(url, headers=headers)
-	print(request.text, file=open('my_router_responses/' + endpoint, "a"))
+	url='https://192.168.1.1/data/'+endpoint
+	request = session.get(url, headers=headers,verify=False)
+	pretty_json = json.dumps(json.loads(request.text),indent=4)
+	print(pretty_json, file=open('my_router_responses/' + endpoint, "a"))
